724358d42d8ca077f84c0c831b39cd446908b41e
Geoffrey Ballenden <7225070+ZA1@users.noreply.github.com>
2021-12-09 08:22:33 +1300

    Number editors on sliders

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const videoRef = useRef();
   const [error, setError] = useState();
   const [caps, setCaps] = useState({});
+  const [update, setUpdate] = useState(1);
   const [constraints, setConstraints] = useState({
     audio: false,
     video: { width: 1280, height: 720, facingMode: "user" }
@@ -36,16 +37,20 @@ function App() {
       const video = {...constraints.video};
       video[name] = parseFloat(event.target.value);
       setConstraints({...constraints, video});
+      setUpdate(update + 1);
     }
 
     const remove = () => {
       const video = {...constraints.video};
       delete video[name];
       setConstraints({...constraints, video});
+      setUpdate(update + 1);
     }
 
-    return <div>
-      <input type="range" max={cap.max} min={cap.min} step={cap.step} value={constraints.video[cap]} onChange={change} />
+    const value = constraints.video[name];
+    return <div key={name + "-edt"}>
+      <input className="range" key={name + "-range"} update={update} type="range" max={cap.max} min={cap.min} step={cap.step} value={value} onChange={change} />
+      <input className="val" key={name + "-num"} update={update} type="number" max={cap.max} min={cap.min} step={cap.step || 0.01} value={value} onChange={change} />
       <button onClick={remove}>X</button>
       </div>;
   }
